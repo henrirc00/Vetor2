9d9f531f1b2c0d51acc04a007bcbd7bc39f39dbb
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-07-02 09:56:37 -0300

    Impressão do conteúdo do vetor com laço For e While

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Vetor2
     {
         static void Main(string[] args)
         {
-            
+          string[] nomes = {"Bruno","Camila","Raphaela","Fábio"};
+
+          for(int y = 3; y >= 0; y--){
+              Console.WriteLine(nomes[y]);
+
+
+          }  
+          Console.WriteLine("_____________________________________________");
+
+          int p = 0;
+          while (p<=3){
+              Console.WriteLine(nomes[p]);
+              p++;
+
+          }
+
+
         }
     }
 }
